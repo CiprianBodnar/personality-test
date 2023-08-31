eeba5b6efe33da750b7aa7de1d9f2bebaa2276c4
bodnar <cbodnar@veridiumid.com>
2023-08-31 16:34:15 +0300

    Personality Test Application: - add public folder

--- src/main/java/com/example/personalitytest/service/UserResultService.java
+++ src/main/java/com/example/personalitytest/service/UserResultService.java
@@ -1,12 +1,15 @@
 package com.example.personalitytest.service;
 
 import com.example.personalitytest.model.Answer;
+import com.example.personalitytest.model.User;
 import com.example.personalitytest.repository.AnswerRepository;
+import com.example.personalitytest.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -16,10 +19,12 @@ import java.util.stream.Collectors;
 public class UserResultService {
 
     private final AnswerRepository answerRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserResultService(AnswerRepository answerRepository) {
+    public UserResultService(AnswerRepository answerRepository, UserRepository userRepository) {
         this.answerRepository = answerRepository;
+        this.userRepository = userRepository;
     }
 
     public String getUserResult(String userId) {
@@ -31,7 +36,14 @@ public class UserResultService {
         for (Answer answer : allByUserId) {
             answersLenght = answersLenght + answer.getAnswer().length();
         }
-        return calculateResult(answersLenght);
+        String s = calculateResult(answersLenght);
+        Optional<User> byId = userRepository.findById(userIdAsLong);
+        if(byId.isPresent()) {
+            User user = byId.get();
+            user.setResult(s);
+            userRepository.save(user);
+        }
+        return s;
     }
 
     public String calculateResult(int answersLength) {
